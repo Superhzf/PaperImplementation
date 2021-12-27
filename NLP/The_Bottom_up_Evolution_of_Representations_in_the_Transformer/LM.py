eec5cb38c329a7309f86f9ab216eb1217825a360
Superhzf <superhzf@hotmail.com>
2021-12-26 19:57:08 -0600

    type: feat
    Debug the LM model file

--- NLP/The_Bottom_up_Evolution_of_Representations_in_the_Transformer/LM.py
+++ NLP/The_Bottom_up_Evolution_of_Representations_in_the_Transformer/LM.py
@@ -1,21 +1,28 @@
 # https://github.com/pytorch/examples/tree/master/word_language_model
 # https://pytorch.org/tutorials/beginner/transformer_tutorial.html
-from model import batchify, train, export_onnx, bptt
+from model import batchify, train, export_onnx, bptt, TransformerModel, evaluate
 from data_preprocessing import Corpus
+import torch
+from torch import nn
+import time
+import math
+import copy
 
+# Set the random seed manually for reproducibility.
+torch.manual_seed(1234)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 data_source = "./data/"
+models_folder = './TrainedModels/'
 # set up the the default size
 batch_size = 20
 eval_batch_size = 10
 # In development mode, I use a small dataset for faster iteration.
-corpus = Corpus(path=data_source,development_model=True)
+corpus = Corpus(path=data_source,development_mode=True)
 
 train_data = batchify(corpus.train, batch_size)
-val_data = batchify(corpus.val, batch_size)
+val_data = batchify(corpus.val, eval_batch_size)
 ntokens = len(corpus.dictionary)
-ntokens = len(vocab)
 emsize = 200
 d_hid = 200
 nlayers = 2
@@ -29,14 +36,14 @@ optimizer = torch.optim.SGD(model.parameters(), lr=lr)
 scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1.0, gamma=0.95)
 
 best_val_loss = float('inf')
-epochs = 3
+epochs = 1
 best_model = None
 
 # train the model
 for epoch in range(1, epochs + 1):
     epoch_start_time = time.time()
-    train(model)
-    val_loss = evaluate(model, val_data)
+    train(model,train_data, criterion, ntokens, optimizer)
+    val_loss = evaluate(model, val_data, ntokens, criterion)
     val_ppl = math.exp(val_loss)
     elapsed = time.time() - epoch_start_time
     print('-' * 89)
@@ -49,12 +56,5 @@ for epoch in range(1, epochs + 1):
         best_model = copy.deepcopy(model)
     scheduler.step()
 
-test_loss = evaluate(best_model, test_data)
-test_ppl = math.exp(test_loss)
-print('=' * 89)
-print(f'| End of training | test loss {test_loss:5.2f} | '
-      f'test ppl {test_ppl:8.2f}')
-print('=' * 89)
-
 # Export the model in ONNX format.
-export_onnx('./', batch_size=1, seq_len=bptt)
+export_onnx(f'{models_folder}LM_model.onnx', batch_size=1, seq_len=bptt,model=best_model)
