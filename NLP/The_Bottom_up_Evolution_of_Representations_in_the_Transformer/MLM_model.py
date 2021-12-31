eeae0f598dcdb1e152e273466d756e16456b81e7
Superhzf <superhzf@hotmail.com>
2021-12-30 18:20:01 -0600

    type: feat
    develop the model file for the MLM task

---
+++ NLP/The_Bottom_up_Evolution_of_Representations_in_the_Transformer/MLM_model.py
@@ -0,0 +1,121 @@
+import math
+from typing import Tuple
+
+import torch
+from torch import nn, Tensor
+import torch.nn.functional as F
+from torch.nn import TransformerEncoder, TransformerEncoderLayer
+from torch.utils.data import dataset
+
+from torchtext.data.utils import get_tokenizer
+from torchtext.vocab import build_vocab_from_iterator
+
+import copy
+import time
+import os
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model, dropout=0.1, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + self.pe[:x.size(0), :]
+        return self.dropout(x)
+
+class TransformerModel(nn.Module):
+
+    def __init__(self, ntoken, ninp, nhead, nhid, nlayers, dropout=0.5):
+        super(TransformerModel, self).__init__()
+        self.model_type = 'Transformer'
+        self.pos_encoder = PositionalEncoding(ninp, dropout)
+        encoder_layers = TransformerEncoderLayer(ninp, nhead, nhid, dropout, batch_first=True)
+        self.transformer_encoder = TransformerEncoder(encoder_layers, nlayers)
+        self.encoder = nn.Embedding(ntoken, ninp)
+        self.ninp = ninp
+        self.decoder = nn.Linear(ninp, ntoken)
+
+        self.init_weights()
+
+    def generate_square_subsequent_mask(self, sz):
+        mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
+        mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
+        return mask
+
+    def init_weights(self):
+        initrange = 0.1
+        self.encoder.weight.data.uniform_(-initrange, initrange)
+        self.decoder.bias.data.zero_()
+        self.decoder.weight.data.uniform_(-initrange, initrange)
+
+    def forward(self, src, src_mask):
+        src = self.encoder(src) * math.sqrt(self.ninp)
+        src = self.pos_encoder(src)
+        output = self.transformer_encoder(src, src_mask)
+        output = self.decoder(output)
+        return output
+
+
+def train(model, dataloader, criterion, ntokens, optimizer, scheduler, epoch):
+    model.train()
+    total_loss = 0
+    log_interval = 200
+    start_time = time.time()
+    i=0
+    for batch in dataloader:
+        print(f"{i}-th sample in train mode")
+        input = batch['input_ids'].clone()
+        src_mask = model.generate_square_subsequent_mask(batch['input_ids'].size(1))
+        rand_value = torch.rand(batch.input_ids.shape)
+        rand_mask = (rand_value < 0.15) * (input != 101) * (input != 102) * (input != 0)
+        mask_idx=(rand_mask.flatten() == True).nonzero().view(-1)
+        input = input.flatten()
+        input[mask_idx] = 103
+        input = input.view(batch['input_ids'].size())
+
+        out = model(input.to(device), src_mask.to(device))
+        loss = criterion(out.view(-1, ntokens), batch['input_ids'].view(-1).to(device))
+        total_loss += loss
+
+        optimizer.zero_grad()
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
+        optimizer.step()
+
+        total_loss += loss.item()
+        i+=1
+
+    return total_loss/len(dataloader)
+
+def evaluate(model: nn.Module, dataloader, ntokens: int, criterion) -> float:
+    model.eval()  # turn on evaluation mode
+    total_loss = 0.
+    i=0
+    with torch.no_grad():
+        for batch in dataloader:
+            print(f"{i}-th sample in evaluate mode")
+            input = batch['input_ids'].clone()
+            src_mask = model.generate_square_subsequent_mask(batch['input_ids'].size(1))
+            rand_value = torch.rand(batch.input_ids.shape)
+            rand_mask = (rand_value < 0.15) * (input != 101) * (input != 102) * (input != 0)
+            mask_idx=(rand_mask.flatten() == True).nonzero().view(-1)
+            input = input.flatten()
+            input[mask_idx] = 103
+            input = input.view(batch['input_ids'].size())
+
+            out = model(input.to(device), src_mask.to(device))
+            loss = criterion(out.view(-1, ntokens), batch['input_ids'].view(-1).to(device))
+
+            total_loss += loss.item()
+            i+=1
+    return total_loss / (len(dataloader) - 1)
