19e359f5d3fdfd5e87d419bdf22e0caae0e23d1e
Superhzf <superhzf@hotmail.com>
2022-01-04 16:12:41 -0600

    type: refactor
    Refactor the PositionalEncoding class for the MT task for unification

--- NLP/The_Bottom_up_Evolution_of_Representations_in_the_Transformer/MT_model.py
+++ NLP/The_Bottom_up_Evolution_of_Representations_in_the_Transformer/MT_model.py
@@ -30,10 +30,9 @@ class PositionalEncoding(nn.Module):
         super(PositionalEncoding, self).__init__()
         den = torch.exp(- torch.arange(0, emb_size, 2)* math.log(10000) / emb_size)
         pos = torch.arange(0, maxlen).reshape(maxlen, 1)
-        pos_embedding = torch.zeros((maxlen, emb_size))
-        pos_embedding[:, 0::2] = torch.sin(pos * den)
-        pos_embedding[:, 1::2] = torch.cos(pos * den)
-        pos_embedding = pos_embedding.unsqueeze(-2)
+        pos_embedding = torch.zeros((maxlen, 1, emb_size))
+        pos_embedding[:, 0, 0::2] = torch.sin(pos * den)
+        pos_embedding[:, 0, 1::2] = torch.cos(pos * den)
 
         self.dropout = nn.Dropout(dropout)
         self.register_buffer('pos_embedding', pos_embedding)
