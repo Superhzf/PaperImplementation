3bd330727e1ec61f641f5cd1a7d4491217378b69
Superhzf <superhzf@hotmail.com>
2022-01-04 16:06:01 -0600

    type: refactor
    Refactor the PositionalEncoding class for the MLM task for unification

--- NLP/The_Bottom_up_Evolution_of_Representations_in_the_Transformer/MLM_model.py
+++ NLP/The_Bottom_up_Evolution_of_Representations_in_the_Transformer/MLM_model.py
@@ -21,12 +21,11 @@ class PositionalEncoding(nn.Module):
         super(PositionalEncoding, self).__init__()
         self.dropout = nn.Dropout(p=dropout)
 
-        pe = torch.zeros(max_len, d_model)
+        pe = torch.zeros(max_len, 1, d_model)
         position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
         div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
-        pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
-        pe = pe.unsqueeze(0).transpose(0, 1)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
         self.register_buffer('pe', pe)
 
     def forward(self, x):
