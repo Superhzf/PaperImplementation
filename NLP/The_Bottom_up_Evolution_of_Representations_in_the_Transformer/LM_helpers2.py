dee7476dea7611203cb0e0229ef1d0a29c72dbb1
Superhzf <superhzf@hotmail.com>
2022-01-09 18:46:09 -0600

    type: feat
    Refactor the LM task in order to use the BPE encoding method

---
+++ NLP/The_Bottom_up_Evolution_of_Representations_in_the_Transformer/LM_helpers2.py
@@ -0,0 +1,95 @@
+import math
+from typing import Tuple
+
+import torch
+from torch import nn, Tensor
+import torch.nn.functional as F
+from torch.utils.data import dataset
+from models import generate_square_subsequent_mask
+
+import copy
+import time
+import os
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+# bptt is actually the intended sequence length
+bptt = 100
+
+
+def batchify(data: Tensor, bsz: int) -> Tensor:
+    """Divides the data into bsz separate sequences, removing extra elements
+    that wouldn't cleanly fit.
+
+    Args:
+        data: Tensor, shape [N]
+        bsz: int, batch size
+
+    Returns:
+        Tensor of shape [N // bsz, bsz]
+    """
+    seq_len = data.size(0) // bsz
+    data = data[:seq_len * bsz]
+    data = data.view(bsz, seq_len).t().contiguous()
+    return data.to(device)
+
+def get_batch(source: Tensor, i: int) -> Tuple[Tensor, Tensor]:
+    """
+    Args:
+        source: Tensor, shape [full_seq_len, batch_size]
+        i: int
+
+    Returns:
+        tuple (data, target), where data has shape [seq_len, batch_size] and
+        target has shape [seq_len * batch_size]
+    """
+    seq_len = min(bptt, len(source) - 1 - i)
+    data = source[i:i+seq_len]
+    target = source[i+1:i+1+seq_len].view(-1)
+    return data, target
+
+def train_epoch(model: nn.Module, train_data, criterion, ntokens, optimizer,epoch) -> None:
+    model.train()  # turn on train mode
+    total_loss = 0.
+    log_interval = 100
+    start_time = time.time()
+    src_mask = generate_square_subsequent_mask(bptt).to(device)
+
+    num_batches = len(train_data) // bptt
+    for batch, i in enumerate(range(0, len(train_data) - 1, bptt)):
+        data, targets = get_batch(train_data, i)
+        seq_len = data.size(0)
+        if seq_len != bptt:  # only on last batch
+            src_mask = src_mask[:seq_len, :seq_len]
+        output = model(data, src_mask)
+        loss = criterion(output.view(-1, ntokens), targets)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step_and_update_lr()
+        # print(f"This is the {batch}th batch")
+        total_loss += loss.item()
+        if batch % log_interval == 0 and batch > 0:
+            lr = optimizer._optimizer.param_groups[0]['lr']
+            ms_per_batch = (time.time() - start_time) * 1000 / log_interval
+            cur_loss = total_loss / log_interval
+            ppl = math.exp(cur_loss)
+            print(f'| epoch {epoch:3d} | {batch:5d}/{num_batches:5d} batches | '
+                  f'lr {lr:02.2f} | ms/batch {ms_per_batch:5.2f} | '
+                  f'loss {cur_loss:5.2f} | ppl {ppl:8.2f}')
+            total_loss = 0
+            start_time = time.time()
+
+def evaluate(model: nn.Module, eval_data: Tensor, ntokens: int, criterion) -> float:
+    model.eval()  # turn on evaluation mode
+    total_loss = 0.
+    src_mask = generate_square_subsequent_mask(bptt).to(device)
+    with torch.no_grad():
+        for i in range(0, eval_data.size(0) - 1, bptt):
+            data, targets = get_batch(eval_data, i)
+            seq_len = data.size(0)
+            if seq_len != bptt:
+                src_mask = src_mask[:seq_len, :seq_len]
+            output = model(data, src_mask)
+            output_flat = output.view(-1, ntokens)
+            total_loss += seq_len * criterion(output_flat, targets).item()
+    return total_loss / (len(eval_data) - 1)
---
+++ NLP/The_Bottom_up_Evolution_of_Representations_in_the_Transformer/LM_main2.py
@@ -0,0 +1,44 @@
+from timeit import default_timer as timer
+from LM_helpers2 import train_epoch,evaluate, batchify
+import pickle
+from torchtext.legacy.data import Dataset,BucketIterator
+import torch
+from data_preprocessing2 import SAVE_DATA_SRC, SAVE_DATA_LM_TRAIN
+from data_preprocessing2 import DATA_DIR, PAD_WORD
+from models import D_MODEL, FFN_HID_DIM, NLAYERS, NHEAD, BATCH_SIZE, DROPOUT, EPOCHS
+from models import TransformerModel, LOSS_FN, ScheduledOptim
+import os
+import time
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+vocab_pkl_src = os.path.join(DATA_DIR, SAVE_DATA_SRC)
+train_pkl = os.path.join(DATA_DIR, SAVE_DATA_LM_TRAIN)
+
+field_src = pickle.load(open(vocab_pkl_src, 'rb'))
+train_examples = pickle.load(open(train_pkl, 'rb'))
+
+fields = {'text':field_src}
+train = Dataset(examples=train_examples, fields=fields)
+train_iter = BucketIterator(train, batch_size=1, device=device, train=True)
+train_iter = batchify(list(train_iter)[0].text, BATCH_SIZE)
+
+src_vocab_size = len(field_src.vocab)
+model=TransformerModel(src_vocab_size=src_vocab_size,
+                       d_model=D_MODEL,
+                       nhead=NHEAD,
+                       dim_feedforward=FFN_HID_DIM,
+                       num_encoder_layer=NLAYERS,
+                       dropout=DROPOUT)
+criterion = LOSS_FN()
+optimizer = ScheduledOptim(model.parameters())
+# train the model
+for epoch in range(1, EPOCHS + 1):
+    epoch_start_time = time.time()
+    train_epoch(model,train_iter, criterion, src_vocab_size, optimizer, epoch)
+    train_loss = evaluate(model, train_iter, src_vocab_size, criterion)
+    train_ppl = math.exp(train_loss)
+    elapsed = time.time() - epoch_start_time
+    print('-' * 89)
+    print(f'| end of epoch {epoch:3d} | time: {elapsed:5.2f}s | '
+          f'train loss {train_loss:5.2f} | train ppl {train_ppl:8.2f}')
+    print('-' * 89)
