1dd09ee40615ecd000bcfb2dc456dd9eeef8dc80
Superhzf <superhzf@hotmail.com>
2022-01-12 11:01:52 -0600

    type: refactor
    refactor the code of the LM task to use the source side of the MT task 
    as the training instances

--- NLP/The_Bottom_up_Evolution_of_Representations_in_the_Transformer/LM_helpers.py
+++ NLP/The_Bottom_up_Evolution_of_Representations_in_the_Transformer/LM_helpers.py
@@ -1,94 +1,62 @@
-import math
-from typing import Tuple
-
+from torch import Tensor
 import torch
-from torch import nn, Tensor
-import torch.nn.functional as F
-from torch.utils.data import dataset
+import torch.nn as nn
+import math
+from torch.utils.data import DataLoader
 from models import generate_square_subsequent_mask
-
-import copy
 import time
-import os
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-# bptt is actually the intended sequence length
-bptt = 100
 
 
-def batchify(data: Tensor, bsz: int) -> Tensor:
-    """Divides the data into bsz separate sequences, removing extra elements
-    that wouldn't cleanly fit.
-
-    Args:
-        data: Tensor, shape [N]
-        bsz: int, batch size
-
-    Returns:
-        Tensor of shape [N // bsz, bsz]
-    """
-    seq_len = data.size(0) // bsz
-    data = data[:seq_len * bsz]
-    data = data.view(bsz, seq_len).t().contiguous()
-    return data.to(device)
-
-def get_batch(source: Tensor, i: int) -> Tuple[Tensor, Tensor]:
-    """
-    Args:
-        source: Tensor, shape [full_seq_len, batch_size]
-        i: int
-
-    Returns:
-        tuple (data, target), where data has shape [seq_len, batch_size] and
-        target has shape [seq_len * batch_size]
-    """
-    seq_len = min(bptt, len(source) - 1 - i)
-    data = source[i:i+seq_len]
-    target = source[i+1:i+1+seq_len].view(-1)
-    return data, target
-
-def train_epoch(model: nn.Module, train_data, criterion, ntokens, optimizer,epoch, sync_every_steps) -> None:
-    model.train()  # turn on train mode
-    total_loss = 0.
-    src_mask = generate_square_subsequent_mask(bptt).to(device)
-    num_batches = len(train_data) // bptt
+def train_epoch(model: nn.Module, train_iter, criterion, ntokens, optimizer,epoch, sync_every_steps):
+    model.train()
+    losses=0
+    i=1
     optimizer.zero_grad()
-    for batch, i in enumerate(range(0, len(train_data) - 1, bptt)):
+    for batch in train_iter:
         start_time = time.time()
-        data, targets = get_batch(train_data, i)
-        seq_len = data.size(0)
-        if seq_len != bptt:  # only on last batch
-            src_mask = src_mask[:seq_len, :seq_len]
-        output = model(data, src_mask)
-        loss = criterion(output.view(-1, ntokens), targets)
+        src_seq = batch.src
+        input = src_seq[:-1].to(device)
+        target = src_seq[1:].view(-1).to(device)
+        seq_len=input.size(0)
+        src_mask=generate_square_subsequent_mask(seq_len).to(device)
+        output = model(input, src_mask)
+        loss = criterion(output.view(-1, ntokens), target)
         curr_loss=loss.item()
         loss = loss/sync_every_steps
         loss.backward()
 
-        if (batch+1)%sync_every_steps == 0:
+        if i%sync_every_steps == 0:
             optimizer.step_and_update_lr()
             optimizer.zero_grad()
+            print("Great, parameters have been updated")
 
-        total_loss += curr_loss
-        s_per_batch = (time.time() - start_time)
+        losses += curr_loss
+        # print the information at each batch
+        s_this_batch=(time.time() - start_time)
         curr_ppl = math.exp(curr_loss)
-        print(f'| epoch {epoch:3d} | {batch:5d}th batch | '
-              f's/batch {s_per_batch:5.2f} | '
-              f'loss {curr_loss:5.2f} | ppl {curr_ppl:8.2f}')
-        start_time = time.time()
-    return total_loss/len(train_data)
+        print(f'| epoch {epoch:3d} | {i:5d} batch | '
+                  f's/batch {s_this_batch:5.2f} | '
+                  f'loss {curr_loss:5.2f} | ppl {curr_ppl:8.2f}')
+        i+=1
 
-def evaluate(model: nn.Module, eval_data: Tensor, ntokens: int, criterion) -> float:
-    model.eval()  # turn on evaluation mode
-    total_loss = 0.
-    src_mask = generate_square_subsequent_mask(bptt).to(device)
-    with torch.no_grad():
-        for i in range(0, eval_data.size(0) - 1, bptt):
-            data, targets = get_batch(eval_data, i)
-            seq_len = data.size(0)
-            if seq_len != bptt:
-                src_mask = src_mask[:seq_len, :seq_len]
-            output = model(data, src_mask)
-            output_flat = output.view(-1, ntokens)
-            total_loss += seq_len * criterion(output_flat, targets).item()
-    return total_loss / len(eval_data)
+    return losses / len(train_iter)
+
+def evaluate(model: nn.Module, eval_data: Tensor, ntokens: int, criterion):
+    model.eval()
+    losses = 0
+    i=1
+    for batch in eval_data:
+        src = batch.src
+        input = src_seq[:-1].to(device)
+        target = src_seq[1:].view(-1).to(device)
+        seq_len=input.size(0)
+        src_mask=generate_square_subsequent_mask(seq_len).to(device)
+        output = model(data, src_mask)
+        loss = criterion(output.view(-1, ntokens), targets)
+        losses += loss.item()
+        print(f"Round {i} in the validation stage")
+        i+=1
+
+    return losses / len(val_iter)
--- NLP/The_Bottom_up_Evolution_of_Representations_in_the_Transformer/LM_main.py
+++ NLP/The_Bottom_up_Evolution_of_Representations_in_the_Transformer/LM_main.py
@@ -1,19 +1,20 @@
 # Modified from
-# https://pytorch.org/tutorials/beginner/transformer_tutorial.html
-from LM_helpers import train_epoch,evaluate, batchify
+# https://pytorch.org/tutorials/beginner/translation_transformer.html
+from timeit import default_timer as timer
+from LM_helpers import train_epoch,evaluate
 import pickle
 from torchtext.legacy.data import Dataset,BucketIterator
 import torch
-from data_preprocessing import SAVE_VOCAB_SRC, SAVE_DATA_LM_TRAIN, SAVE_DATA_LM_VAL
-from data_preprocessing import DATA_DIR_DEV, DATA_DIR_FULL, DEVELOPMENT_MODE
+from data_preprocessing import DEVELOPMENT_MODE
+from data_preprocessing import SAVE_VOCAB_SRC, SAVE_VOCAB_TRG, SAVE_DATA_MT_TRAIN, SAVE_DATA_MT_VAL
+from data_preprocessing import DATA_DIR_DEV, DATA_DIR_FULL, PAD_WORD
 from models import D_MODEL, FFN_HID_DIM, NLAYERS, NHEAD, BATCH_SIZE, DROPOUT
 from models import EPOCHS_DEV, EPOCHS_FULL, SYNC_EVERY_BATCH_DEV, SYNC_EVERY_BATCH_FULL
 from models import TransformerModel, LOSS_FN, ScheduledOptim
 import os
-import time
 import math
 
-device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 if DEVELOPMENT_MODE:
     DATA_DIR=DATA_DIR_DEV
     EPOCHS=EPOCHS_DEV
@@ -23,22 +24,27 @@ else:
     EPOCHS=EPOCHS_FULL
     SYNC_EVERY_STEPS=SYNC_EVERY_BATCH_FULL
 
-vocab_pkl_src=os.path.join(DATA_DIR, SAVE_VOCAB_SRC)
-train_pkl=os.path.join(DATA_DIR, SAVE_DATA_LM_TRAIN)
-valid_pkl=os.path.join(DATA_DIR, SAVE_DATA_LM_VAL)
 
-field_src=pickle.load(open(vocab_pkl_src, 'rb'))
-train_examples=pickle.load(open(train_pkl, 'rb'))
-valid_examples=pickle.load(open(valid_pkl, 'rb'))
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+vocab_pkl_src = os.path.join(DATA_DIR, SAVE_VOCAB_SRC)
+vocab_pkl_trg = os.path.join(DATA_DIR, SAVE_VOCAB_TRG)
+train_pkl = os.path.join(DATA_DIR, SAVE_DATA_MT_TRAIN)
+valid_pkl = os.path.join(DATA_DIR, SAVE_DATA_MT_VAL)
+
+field_src = pickle.load(open(vocab_pkl_src, 'rb'))
+field_trg = pickle.load(open(vocab_pkl_trg, 'rb'))
+train_examples = pickle.load(open(train_pkl, 'rb'))
+valid_examples = pickle.load(open(valid_pkl, 'rb'))
+
+src_pad_idx = field_src.vocab.stoi[PAD_WORD]
 
 src_vocab_size = len(field_src.vocab)
-fields = {'text':field_src}
+
+fields = {'src':field_src , 'trg':field_trg}
 train = Dataset(examples=train_examples, fields=fields)
 valid = Dataset(examples=valid_examples, fields=fields)
-train_iter = BucketIterator(train, batch_size=1, device=device, train=True)
-train_iter = batchify(list(train_iter)[0].text, BATCH_SIZE)
-valid_iter = BucketIterator(valid, batch_size=1, device=device)
-valid_iter = batchify(list(valid_iter)[0].text, BATCH_SIZE)
+train_iter = BucketIterator(train, batch_size=BATCH_SIZE, device=device, train=True)
+valid_iter = BucketIterator(valid, batch_size=BATCH_SIZE, device=device)
 
 model=TransformerModel(src_vocab_size=src_vocab_size,
                        d_model=D_MODEL,
@@ -46,23 +52,22 @@ model=TransformerModel(src_vocab_size=src_vocab_size,
                        dim_feedforward=FFN_HID_DIM,
                        num_encoder_layer=NLAYERS,
                        dropout=DROPOUT)
-criterion = LOSS_FN()
-optimizer = ScheduledOptim(model.parameters())
+
+model=model.to(device)
+loss_fn=LOSS_FN()
+optimizer=ScheduledOptim(model.parameters())
 best_loss=float('inf')
 best_ppl=None
 best_model =None
-# train the model
-for epoch in range(1, EPOCHS + 1):
-    start_time = time.time()
-    train_loss=train_epoch(model,train_iter, criterion, src_vocab_size, optimizer, epoch, SYNC_EVERY_STEPS)
-    end_time = time.time()
-    train_ppl =math.exp(train_loss)
-    valid_loss = evaluate(model, valid_iter, src_vocab_size, criterion)
-    valid_ppl = math.exp(valid_loss)
-    elapsed = end_time - start_time
+for epoch in range(1, EPOCHS+1):
+    start_time = timer()
+    train_loss=train_epoch(model, train_iter, loss_fn, src_vocab_size, optimizer, epoch, SYNC_EVERY_STEPS)
+    end_time = timer()
+    valid_loss = evaluate(model, valid_iter, src_vocab_size, loss_fn)
+    valid_ppl = math.exp(val_loss)
     if valid_loss<best_loss:
         best_loss=valid_loss
-        best_ppl=valid_ppl
+        best_ppl=val_ppl
         best_model=model
     print(f"Epoch: {epoch}|Train loss: {train_loss:.2f}|"
             f"Current validation loss: {valid_loss:.2f}|"
