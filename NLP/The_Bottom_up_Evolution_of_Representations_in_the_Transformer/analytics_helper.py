1db2194c091773a19e9e95ed30d23fc67f802912
Superhzf <superhzf@hotmail.com>
2022-01-17 21:04:23 -0600

    type: refactor
    Return the ids of the N most frequent tokens

--- NLP/The_Bottom_up_Evolution_of_Representations_in_the_Transformer/analytics_helper.py
+++ NLP/The_Bottom_up_Evolution_of_Representations_in_the_Transformer/analytics_helper.py
@@ -1,17 +1,20 @@
-
 def MostFreqToken(field_src, N):
     """
-    It will return the N most frequent sub-words.
+    It will return the ids of the N most frequent sub-words.
     ----------------
     field_src: torchtext.legacy.data.Field
         This is the vocab class by Pytorch
     N: int
-        The N most frequent sub-words will be returned
+        The ids of the N most frequent sub-words will be returned
     """
     sorted_vocab={k: v for k, v in sorted(field_src.vocab.freqs.items(), key=lambda item: item[1],reverse=True)}
     frequent_vocab=[]
+    frequent_ids=[]
     for count, (this_vocab, freq) in enumerate(sorted_vocab.items()):
         if count < N:
             frequent_vocab.append(this_vocab)
         else:
-            return frequent_vocab
+            break
+    for this_vocab in frequent_vocab:
+        frequent_ids.append(field_src.vocab.stoi[this_vocab])
+    return frequent_ids
