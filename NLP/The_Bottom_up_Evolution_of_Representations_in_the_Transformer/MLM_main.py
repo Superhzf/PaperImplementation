bf75b1d8aeeca3e7a642d68a34a57573b095bd8e
Superhzf <superhzf@hotmail.com>
2022-02-20 21:42:35 -0600

    type: refactor
    Use the SepTransformerModel class so that we can modify the mask matrix

--- NLP/The_Bottom_up_Evolution_of_Representations_in_the_Transformer/MLM_main.py
+++ NLP/The_Bottom_up_Evolution_of_Representations_in_the_Transformer/MLM_main.py
@@ -11,7 +11,7 @@ from data_preprocessing import DATA_DIR_DEV, DATA_DIR_FULL, PAD_WORD
 from data_preprocessing import SAVE_MODEL_PATH, NO_BETTER_THAN_ROUND
 from models import D_MODEL, FFN_HID_DIM, NLAYERS, NHEAD, BATCH_SIZE, DROPOUT
 from models import EPOCHS_DEV, EPOCHS_FULL, SYNC_EVERY_BATCH_DEV, SYNC_EVERY_BATCH_FULL
-from models import TransformerModel, LOSS_FN, ScheduledOptim
+from models import SepTransformerModel, LOSS_FN, ScheduledOptim
 from models import MLM_NAME
 import os
 import time
@@ -51,7 +51,7 @@ train_iter = BucketIterator(train, batch_size=BATCH_SIZE, device=device, train=T
 valid = Dataset(examples=valid_examples, fields=fields)
 valid_iter = BucketIterator(valid, batch_size=BATCH_SIZE, device=device, shuffle=False)
 
-model = TransformerModel(src_vocab_size=src_vocab_size,
+model = SepTransformerModel(src_vocab_size=src_vocab_size,
                          d_model=D_MODEL,
                          nhead=NHEAD,
                          dim_feedforward=FFN_HID_DIM,
