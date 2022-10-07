177e99ac92fdb30865c09a6ffb8dd26582193ad1
Superhzf <superhzf@hotmail.com>
2022-10-07 13:43:28 -0500

    type: refactor
    Modify the running path

--- Resnet/script_main.sh
+++ Resnet/script_main.sh
@@ -7,5 +7,5 @@
 #SBATCH --gres=gpu:rtx_6000:1
 
 module load ml-gpu
-cd /work/LAS/cjquinn-lab/zefuh/COMS673/CS673/HW2/code
+cd /work/LAS/cjquinn-lab/zefuh/COMS673/PaperImplementation/Resnet
 ml-gpu /work/LAS/cjquinn-lab/zefuh/COMS673/DL_env/bin/python main.py -seed 2022 -resnet_version 2 -resnet_size 3 -num_classes 10 -num_epochs 164 -batch 128 -lr 0.1 -> log_2022_v2
